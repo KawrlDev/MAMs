eba337c1691598773457b13d510be860fcea0009
KawrlDev <ninjastarz2243@gmail.com>
2026-02-16 16:49:56 +0800

    e

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: '',
   withCredentials: true,
   withXSRFToken: true, // Add this
   headers: {
